bad59fedc02d69eb227492ad3f618aee73dc00d0
Jeremy Hilado <jhilado89@gmail.com>
2020-05-13 09:09:03 -0700

    added component files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, createContext} from 'react';
 import './App.css';
 
+
 function App() {
+  const [userInfo, setUserInfo] = useState(() => {
+    const result = localStorage.getItem('user')
+    return result ? JSON.parse(result) : []
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RundownContext.Provider value={
+        {
+          setUserInfo,
+          userInfo
+        }
+      }>
+        <Main/>
+      </RundownContext.Provider>
     </div>
   );
 }
 
-export default App;
+export default App
